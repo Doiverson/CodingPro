8a5361b2cad0a1774df7fb0834af88294eadc006
SHOSUKE DOI <doiverson.canada@gmail.com>
2018-06-09 17:18:26 -0700

    Finish Fimal Exam of C

--- Practice_C/Dijkstra_Final_Exam/TEST3/main.c
+++ Practice_C/Dijkstra_Final_Exam/TEST3/main.c
@@ -24,7 +24,7 @@ char* getNodes(){
 }
 
 char* getEdges(){
-    return "A_B_10,A_F_6,B_C_6,C_B,10,D_A_3,E_B_5,F_B_10,G_G_0";
+    return "A_B_10,A_F_6,B_C_6,C_B_10,D_A_3,E_B_5,F_B_10,G_G_0";
 }
 
 
@@ -53,25 +53,29 @@ struct node* prev = NULL;
 struct node* p;
 struct Edge* edgepoint = NULL;
 struct Edge* pe;
-struct node* le;
+struct node* Osaka;
+struct Edge* le;
+struct node* Kyoto;
+struct node* Hyogo;
+int total;
 
-void showNodes(struct node *nodes) {
-    printf("Show Nodes-----------\n");
-    while (nodes != NULL) {
-        printf("id = %c\n", nodes->name);
-        //printf("prevNode = %d\n", nodes->prevNode);
-        while (nodes->edge != NULL) {
-            printf("%c %d\n", nodes->edge->to->name, nodes->edge->distance);
-            nodes->edge = nodes->edge->next;
-        }
-        nodes = nodes->next;
-    }
-}
+//void showNodes(struct node *nodes) {
+//    printf("Show Nodes-----------\n");
+//    while (nodes != NULL) {
+//        printf("id = %c\n", nodes->name);
+//        //printf("prevNode = %d\n", nodes->prevNode);
+//        while (nodes->edge != NULL) {
+//            printf("%c %d\n", nodes->edge->to->name, nodes->edge->distance);
+//            nodes->edge = nodes->edge->next;
+//        }
+//        nodes = nodes->next;
+//    }
+//}
 
 int main() {
     
     //Split Inputs
-    int i=0, c=0;
+    int i = 0;
     char source = 'A';
     char destination = 'F';
     char getNodes[] = {'A','B','C','D','E','F','G'};
@@ -80,7 +84,8 @@ int main() {
     char first[] = {'A','A','B','C','D','E','F','G'};
     char second[] = {'B','F','C','B','A','B','B','G'};
     int value[] = {10,6,6,10,3,5,10,0};
- 
+    char path[n+1];
+
     //Create Linked List and Initiallize
     
     for(int i=0; i<n; i++){
@@ -132,38 +137,96 @@ int main() {
             } else {
                 pe = p->edge;
                 while(pe->next!=NULL){
-                pe->next = edgepoint;
+                pe = pe->next ;
                 }
+                pe->next = edgepoint;
         }
 
-        edgepoint->next = NULL;
+        
     }
     
-showNodes(head);
+//showNodes(head);
   
-    // Dijkstra Algorithm
-    
+    // <Dijkstra Algorithm>
+
+    int min = INF;
     p=head;
-    int min = 0;
+    
+    //Find Source
     
     while(p!=NULL){
         if(p->name==source){
             p->cost = 0;
             p->visited = 1;
             break;
+        }
+        p = p->next;
     }
-        
-        le = head;
-        for(int i=0;i>n; i++){
-            if(le->visited!=1 && le->cost > min ){
-                min = le->cost;
-                
+    
+    //Find Minimum Cost for Each Edges
+    
+    Osaka = p;
+    i =0;
+    for(i=1;i<n; i++){
+        le = Osaka->edge;
+        if (le!=NULL) {
+            while(le!=NULL){
+                if(Osaka->cost + le->distance < le->to->cost){
+                    le->to->cost = Osaka->cost + le->distance ;
+                    le->to->from = Osaka;
+                }
+                le = le->next;
+
             }
         }
+        Kyoto = head;
+        min = INF;
+        while(Kyoto!=NULL){
+            if(Kyoto->cost < min+1 && Kyoto->visited != 1 ){
+                min = Kyoto->cost;
+                Osaka = Kyoto;
+            }
+            
+            Kyoto = Kyoto->next;
+            
+        }
+        Osaka->visited = 1;
+        }
     
+    //Find Total Cost of The Shortest Path
     
-//    if(p->name=destination)
-//    if(pe->distance > distance[index] + edge[index][i]){
-//        distance[i] = distance[index] + edge[index][i];
-//        previous[i] = index;
+    Hyogo = head;
+    while(Hyogo->name != destination){
+        Hyogo = Hyogo->next;
+    }
+    total=Hyogo->cost;
+    
+    //Find Which Nodes You Come Throuth
+    
+    i=0;
+    memset(path, '\0', n+1);
+    
+    while(Hyogo->name!=source){
+        path[i] = Hyogo->name;
+        i++;
+        Hyogo = Hyogo->from;
+        if(Hyogo == NULL && Hyogo->name != source){
+            i=0;
+            break;
+        }
+    }
+    if(i>0){
+        path[i] = Hyogo->name;
+    }
+    
+    printf("The shortest path from %c to %c", source, destination);
+    if(i!=0){
+        for(; i>=0; i--){
+            printf("\n --> %c", path[i]);
+        }
+    } else {
+        printf("\nNO PATH");
+    }
+    
+    printf("\nTotal Distance is %d\n", total);
 }
