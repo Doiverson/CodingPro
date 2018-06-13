6c20d5117aff0008b1419a43d5b6505c43467153
SHOSUKE DOI <doiverson.canada@gmail.com>
2018-06-13 13:46:00 -0700

    update Java

---
+++ Practice_Java/src/Student.java
@@ -0,0 +1,35 @@
+public class Student {
+    String firstname;
+    String lastname;
+    String stID;
+    float gpa;
+    array[] course;
+
+
+    public Person(String firstname, String lastname, String stID, float gpa){
+        this.firstname = first;
+        this.lastname = last;
+        this.stId = id;
+        this.gpa = gpa;
+    }
+
+    public String getFirstname(){
+        return first;
+    }
+
+    public String getLastname(){
+        return last;
+    }
+
+    public String getStID(){
+        return id;
+    }
+
+    public float getGpa(){
+        return gpa;
+    }
+
+    
+}
+
+
