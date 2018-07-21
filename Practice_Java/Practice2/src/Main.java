f15947b91d36923cb544601c6cb197cb080f76e9
SHOSUKE DOI <doiverson.canada@gmail.com>
2018-07-21 00:48:51 -0700

    java study

--- Practice_Java/Practice2/src/Main.java
+++ Practice_Java/Practice2/src/Main.java
@@ -1,6 +1,20 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Math.E;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        List<String> list = new ArrayList<String>();
+
+        list.add("Hello");
+        list.add("world");
+        list.add("I am Sho");
+
+        for (String s: list){
+            System.out.println(s);
+        }
     }
 }
