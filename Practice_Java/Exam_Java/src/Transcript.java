a1b792338b0e811dd64665191948aca6be12447c
SHOSUKE DOI <doiverson.canada@gmail.com>
2018-07-26 22:14:09 -0700

    update

--- Practice_Java/Exam_Java/src/Transcript.java
+++ Practice_Java/Exam_Java/src/Transcript.java
@@ -7,6 +7,6 @@ class Transcript {
     }
 
     public void initializeGrades(){
-        float[] arrayA = new float[]{45.6f, 89.0f, 79.1f, 60.6f, 55, 84, 71, 69, 80};
+        float[] arrayA = new float[]{45.6f, 89.0f, 79.1f, 60.6f, 55f, 84f, 71f, 69f, 80f};
     }
 }
