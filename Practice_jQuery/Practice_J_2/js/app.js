4b7d74f4f4078fd7d026f6ff3e28a207209ad0d3
SHOSUKE DOI <doiverson.canada@gmail.com>
2018-08-03 18:27:34 -0700

    update

--- Practice_jQuery/Practice_J_2/js/app.js
+++ Practice_jQuery/Practice_J_2/js/app.js
@@ -1,13 +1,19 @@
 
+// $('.spoiler button').click(function () {
+// });
+
+$('.spoiler').on('click', 'button', function(event){
+    console.log(event.target);
+
+    // $('.spoiler span').show();
+    $(event.target).hide();
+});
+
 const $button = $('<button>Reveal Spoiler</button>');
 $('.spoiler').prepend($button);
 
 
 $('.spoiler span').hide();
 
-$('.spoiler button').on('click', function () {
-    $('.spoiler span').show();
-    $('.spoiler button').hide();
-});
 
 
