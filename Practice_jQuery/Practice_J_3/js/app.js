2113a64a76c6b2f3b028d0730cf609312147d2b4
SHOSUKE DOI <doiverson.canada@gmail.com>
2018-08-23 16:32:19 -0700

    update

--- Practice_jQuery/Practice_J_3/js/app.js
+++ Practice_jQuery/Practice_J_3/js/app.js
@@ -2,6 +2,19 @@
 const $odd = $('a:odd');
 const $secureLinks = $('a[href^="https://"]'); /*$は末尾、^は先頭*/
 const $pdfs = $('a[href$=".pdf"]');
+const $pdfCheckbox = $('<label><input type="checkbox"> Allow PDF downloads</label>');
 
 $secureLinks.attr('target', '_blank');
 $pdfs.attr('download', true);
+
+$secureLinks.addClass('secure')
+$pdfs.addClass('pdf')
+
+$pdfs.on('click', (event)=>{
+    if($(':checked').length === 0){
+        event.preventDefault();
+        alert('Please check the box to allow download')
+    }
+});
+
+$('#links').append($pdfCheckbox);
